72eafd7e6883a74b7a665ac730df5d9650d5da53
trannhusum <transum30052005@gmail.com>
2024-05-10 21:13:46 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadTextFileExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input file path");
+            string path = Console.ReadLine();
+            Console.WriteLine("File path: " + path);
+            ReadTextFileExample example = new ReadTextFileExample();
+            example.ReadTextFile(path);
+            Console.ReadKey();
+        }
+    }
+}
